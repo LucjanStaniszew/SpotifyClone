4fa3efb01a269ca673a06062779d296ec9fea5ac
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-05-15 12:14:13 -0300

    Added dispatch to save album

--- src/Components/Albums/Albums.jsx
+++ src/Components/Albums/Albums.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { saveAlbum } from '../../Redux/actions';
+import { getTokenFromUrl } from '../LogIn/LogIn';
 import './Albums.css';
 
-export default function Albums ({cover, title, date}) {
+export default function Albums ({album, id, cover, title, date}) {
+
+    const logged = getTokenFromUrl();
+    const token = logged.access_token;
+    const dispatch = useDispatch()
+
+    const handleButton = (e) => {
+        dispatch(saveAlbum(e, token ))
+    }
+
     return (
         <div className="albumsHome">
             <img src={cover} alt={title} className="cover"/>
             <h2 className="title">{title}</h2>
             <h5 className="date">Publicado: {date}</h5>
+            <button className="add" key={id} onClick={() => handleButton(album.id)} >+ Add album</button>
         </div>
     )
 
